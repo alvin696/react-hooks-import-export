6451f952de827a61bfccaa420b86f6c359007cf6
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-04-18 14:41:35 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// If "./ColoradoStateParks" has a named export (e.g. namedExport)
+import { namedExport } from "./ColoradoStateParks"; 
+// Use the named export in your component or remove this line if not needed
+
+// If "./ColoradoStateParks" has a default export
 import ColoradoStateParks from "./ColoradoStateParks";
+// Use the default export in your component
+
+// If "./ColoradoStateParks" does not have any export, you can add a default export
+// Example:
+// In ColoradoStateParks.js
+// export default () => {
+//   // Your component implementation here
+// }
+
+
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
